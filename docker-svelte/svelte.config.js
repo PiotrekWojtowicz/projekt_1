7f2c02b8a1df2aa45696b9de6da37f755c06b293
Piotr Wójtowicz <piotrekwjtowicz@yahoo.pl>
2023-12-06 21:13:25 +0100

    generated static content with relative path

--- docker-svelte/svelte.config.js
+++ docker-svelte/svelte.config.js
@@ -3,6 +3,10 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		paths: {
+			// set an empty string or './' as the base path
+			base: '/home/wojtowic_1185346/public_html',
+		},
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
